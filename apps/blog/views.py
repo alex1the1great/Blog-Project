89f7786063f44931fc04841d23005cbb7d3ba68f
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-25 09:05:13 +0545

    feat(share post by an email): create view to send email

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+
+from decouple import config
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -26,3 +30,24 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you to read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}'s comments: {cd['comments']}"
+            sender = config('EMAIL_SENDER')
+
+            send_mail(subject, message, sender, [cd['to']])
+            sent = True
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
--- root/settings/development.py
+++ root/settings/development.py
@@ -10,3 +10,8 @@ MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
