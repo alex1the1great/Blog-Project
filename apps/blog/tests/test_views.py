ce25cdd61423ec4264de3d7c059e1da38a4800a6
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-25 16:20:30 +0545

    Test post detail view

--- apps/blog/tests/test_views.py
+++ apps/blog/tests/test_views.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
+from django.contrib.auth.models import User
 
 from ..views import post_list
+from ..models import Post
 
 
 class PostListTest(TestCase):
@@ -20,3 +22,14 @@ class PostListTest(TestCase):
     def test_post_list_use_correct_view(self):
         view = resolve('/blog/')
         self.assertEqual(view.func.__name__, post_list.__name__)
+
+
+class PostDetailTest(TestCase):
+    def setUp(self):
+        author = User.objects.create_user(username='test_user', password='django123')
+        Post.objects.create(title='Post 1', slug='post-1', body='Post 1 body', author=author)
+
+    def test_post_detail_status_code(self):
+        post = Post.objects.get(id=1)
+        response = self.client.get(post.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
