e7cbdc44dd39389afd059c1f296a6a9fb8d20d63
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-26 09:12:18 +0545

    Remove fields from post model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -26,8 +26,6 @@ class Post(DateTimeFieldModel):
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
